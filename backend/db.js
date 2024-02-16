1551dc5016064eb3a40788a6d5ac8a27f2f5d596
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-02-16 23:03:29 +0530

    Account Schema

--- backend/db.js
+++ backend/db.js
@@ -31,8 +31,23 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+// Accounts schema
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema)
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {
-    User
+    User,
+    Account,
 };
